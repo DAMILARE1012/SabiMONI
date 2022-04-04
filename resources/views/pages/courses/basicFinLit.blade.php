50d76cb5b5b9de44eebb55e187de5b1335b9595f
tech-911 <babatundeolatunji74@gmail.com>
2022-04-04 13:08:32 +0100

    modified courses

--- resources/views/pages/courses/basicFinLit.blade.php
+++ resources/views/pages/courses/basicFinLit.blade.php
@@ -1,74 +1,166 @@
 @extends('layouts.master')
 
 @section('title', 'Basic Fiancial Literacy')
-    
+
 @section('content')
-<style>
-    
-</style>
-    <div id="header-writeup">
-       <div class="container">
-        Basic Financial Literacy
-       </div>
+    <style>
+
+    </style>
+    {{-- ----------Jumbotron Section---------- --}}
+    <div class="header-text mb-5">
+        <div class="container">
+            <h3 style="color: white;"><b>Basic Financial Literacy </b></h3>
+        </div>
     </div>
-    <div class="basic">
-        <div class="row">
-           
-                <div class="col-md-5 col-md-offset-1">
-                    <img src="{{ asset('assets/images/courses-image1.jpg') }}"
-                    class="img-responsive" alt="" width="100%">
-                    <div class="courses-text">
-                        <h3><a href="basicFinLit">Basic Financial Literacy</a></h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, necessitatibus reprehenderit a vitae molestias nesciunt expedita magnam culpa suscipit voluptas possimus laboriosam ducimus architecto aut quia. Nihil consequuntur tenetur perspiciatis? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam corrupti obcaecati aliquid, temporibus perspiciatis nisi nam velit ut mollitia quam, consectetur quis exercitationem neque dolorem rerum voluptate ab deleniti blanditiis!</p>
-                    </div>
-                </div>    
-            <div class="col-md-3 col-md-offset-1 " >
+
+    {{-- ----------Section one---------- --}}
+
+    <div class="row">
+
+        <div class="col-md-5 col-md-offset-1">
+            <img src="{{ asset('assets/images/courses-image1.jpg') }}" alt="" width="100%">
+            <div class="courses-text">
+                <h3><a href="basicFinLit">Basic Financial Literacy</a></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, necessitatibus reprehenderit a
+                    vitae molestias nesciunt expedita magnam culpa suscipit voluptas possimus laboriosam ducimus
+                    architecto aut quia. Nihil consequuntur tenetur perspiciatis? Lorem ipsum dolor sit amet,
+                    consectetur adipisicing elit. Aliquam corrupti obcaecati aliquid, temporibus perspiciatis nisi nam
+                    velit ut mollitia quam, consectetur quis exercitationem neque dolorem rerum voluptate ab deleniti
+                    blanditiis!</p>
+            </div>
+        </div>
+        <div class="col-md-3 col-md-offset-1 ">
+            <div class="courses-card">
+
                 <div class="sanef">
-                    <img src="{{ asset('assets/images/courses-image2.jpg') }}"
-                    class="img-responsive" alt="">
+                    <img src="{{ asset('assets/images/courses-image2.jpg') }}" alt="" width="100%" height="170px">
                 </div>
 
                 <div class="courses-detail">
                     <h3><a href="sanef">SANEF</a></h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit
+                        amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
 
                 <div class="courses-info">
                     <div class="courses-author">
-                            <img src="{{ asset('assets/images/author-image1.jpg') }}"
-                                class="img-responsive" alt="">
-                            <span>General</span>
+                        <img src="{{ asset('assets/images/author-image1.jpg') }}" alt="">
+                        <span>General</span>
                     </div>
                     <div class="courses-price">
-                    <a href="#"><span>Register</span></a>
+                        <a href="#"><span>Register</span></a>
                     </div>
 
                 </div>
-                    <br>
+            </div>
+
+            <div class="courses-card courses-card-1">
+
                 <div class="cflt">
-                    <img src="{{ asset('assets/images/courses-image5.jpg') }}"
-                    class="img-responsive" alt="">
+                    <img src="{{ asset('assets/images/courses-image5.jpg') }}" alt="" width="100%" height="170px">
                 </div>
 
                 <div class="courses-detail">
                     <h3><a href="cflt">CFLT</a></h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit
+                        amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
 
-                    <div class="courses-info">
-                        <div class="courses-author">
-                            <img src="{{ asset('assets/images/author-image1.jpg') }}"
-                                class="img-responsive" alt="">
-                            <span>General</span>
-                        </div>
-                        <div class="courses-price">
-                            <a href="#"><span>Register</span></a>
-                        </div>
-    
+                <div class="courses-info">
+                    <div class="courses-author">
+                        <img src="{{ asset('assets/images/author-image1.jpg') }}" alt="">
+                        <span>General</span>
                     </div>
-            </div>  
-            
+                    <div class="courses-price">
+                        <a href="#"><span>Register</span></a>
+                    </div>
+
+                </div>
+            </div>
+
         </div>
-   
+
     </div>
+    {{-- ----------Section two---------- --}}
+
+
+    <div class="courses-section-2">
+        <div class="container courses-section-2-wrapper">
+
+            <div class="courses-item-1">
+                Is your institution interested in being onboarded as well? <span>Contact us</span> for more information.
+            </div>
+            <div class="courses-button-1">
+                <a href="#"> Register</a>
+
+            </div>
+        </div>
+    </div>
+
+    {{-- ----------Section Three---------- --}}
+
+    <div class="container courses-section-3-wrapper">
+        <div class="row" style="  text-align:center; margin-top: 40px; margin-bottom: 40px">
+            <h1>Assessments And Scoring Policy</h1>
+
+        </div>
+        <div class="row">
+            <div class="col-md-6 ">
+                <div class="cs3-item1">
+                    <img src="{{ asset('assets/images/2.png') }}" alt="" width="150px" height="130px">
+                    <div class="">
+                        <h2 style="margin-top: 0px; margin-bottom: 0px; padding-left: 12px;">Online Assessment
+                        </h2>
+                        <hr style="border: 2px solid white; margin-top: 0px; margin-bottom: 0px">
+                        <p style="padding-left: 12px;">At the end of each module you are asked to pass the final module
+                            assessment with a score of at
+                            least 60%. In case of not passing, you have a final second attempt to retake the assessment
+                            after
+                            studying the module again.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-6 ">
+                <div class="cs3-item1" style="padding-bottom: 20px">
+                    <img src="{{ asset('assets/images/3.png') }}" alt="" width="150px" height="130px">
+                    <div class="">
+                        <h2 style="margin-top: 0px; margin-bottom: 0px; padding-left: 12px;">Certification
+                        </h2>
+                        <hr style="border: 2px solid white; margin-top: 0px; margin-bottom: 0px">
+                        <p style="padding-left: 12px;">Your certificate will be issued after having completed all
+                            assessments successfully.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="row">
+            <div class="cs3-item2">
+                <img src="{{ asset('assets/images/1.png') }}" alt="" width="150px" height="130px">
+                <div class="">
+                    <h2 style="margin-top: 0px; margin-bottom: 0px; padding-left: 12px;">Final Teaching Assessment (For
+                        Trainers Only)
+                    </h2>
+                    <hr style="border: 2px solid white; margin-top: 0px; margin-bottom: 0px">
+                    <p style="padding-left: 12px;">At the end of each module
+                        you are asked to pass the final module
+                        assessment with a score of at
+                        least 60%. In case of not passing, you have a final second attempt to retake the assessment
+                        after
+                        studying the module again.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
 @endsection
--- resources/views/pages/courses/sanef.blade.php
+++ resources/views/pages/courses/sanef.blade.php
@@ -4,70 +4,165 @@
 
 @section('content')
 
-    <div id="header-writeup">
+    <div class="header-text mb-5">
         <div class="container">
-            SANEF
+            <h3 style="color: white;"><b>SANEF Group </b></h3>
         </div>
     </div>
     <div class="basic">
         <div class="row">
-           
-                <div class="col-md-5 col-md-offset-1">
-                    <img src="{{ asset('assets/images/courses-image2.jpg') }}"
-                    class="img-responsive" alt="" width="100%">
-                    <div class="courses-text">
-                        <h3><a href="sanef">SANEF</a></h3>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum ratione obcaecati beatae sapiente eaque, necessitatibus, perferendis consectetur non explicabo vero accusantium provident nesciunt doloribus deleniti cupiditate ipsa quia repudiandae commodi? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi nemo iusto unde officiis quidem? Est voluptatum nostrum autem? Assumenda blanditiis ducimus, tempora incidunt aspernatur optio numquam deserunt quis voluptatibus dolore?.</p>
-                    </div>
-                </div>    
-            <div class="col-md-3 col-md-offset-1" >
-                <div class="clft">
-                    <img src="{{ asset('assets/images/courses-image1.jpg') }}"
-                    class="img-responsive" alt="">   
-                </div>
 
-                <div class="courses-detail">
-                    <h3><a href="basicFinLit">Basic Financial Literacy</a></h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div class="col-md-5 col-md-offset-1">
+                <img src="{{ asset('assets/images/courses-image2.jpg') }}" class="img-responsive" alt="" width="100%">
+                <div class="courses-text">
+                    <h3><a href="sanef">SANEF</a></h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum ratione obcaecati beatae sapiente
+                        eaque, necessitatibus, perferendis consectetur non explicabo vero accusantium provident nesciunt
+                        doloribus deleniti cupiditate ipsa quia repudiandae commodi? Lorem ipsum dolor, sit amet consectetur
+                        adipisicing elit. Animi nemo iusto unde officiis quidem? Est voluptatum nostrum autem? Assumenda
+                        blanditiis ducimus, tempora incidunt aspernatur optio numquam deserunt quis voluptatibus dolore?.
+                    </p>
                 </div>
+            </div>
+            <div class="col-md-3 col-md-offset-1 ">
+                <div class="courses-card">
 
-                <div class="courses-info">
-                    <div class="courses-author">
-                            <img src="{{ asset('assets/images/author-image1.jpg') }}"
-                                class="img-responsive" alt="">
-                            <span>General</span>
-                    </div>
-                    <div class="courses-price">
-                    <a href="#"><span>Register</span></a>
+                    <div class="sanef">
+                        <img src="{{ asset('assets/images/courses-image1.jpg') }}" alt="" width="100%" height="170px">
                     </div>
 
-                </div>
-                    <br>
-                <div class="sanef">
-                    <img src="{{ asset('assets/images/courses-image5.jpg') }}"
-                    class="img-responsive" alt="">    
-                </div>
-
-                <div class="courses-detail">
-                    <h3><a href="cflt">CFLT</a></h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
+                    <div class="courses-detail">
+                        <h3><a href="basicFinLit">Basic Financial Literacy</a></h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit
+                            amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
 
                     <div class="courses-info">
                         <div class="courses-author">
-                            <img src="{{ asset('assets/images/author-image1.jpg') }}"
-                                class="img-responsive" alt="">
+                            <img src="{{ asset('assets/images/author-image1.jpg') }}" alt="">
                             <span>General</span>
                         </div>
                         <div class="courses-price">
                             <a href="#"><span>Register</span></a>
                         </div>
-    
+
                     </div>
-            </div>  
-            
+                </div>
+
+                <div class="courses-card courses-card-1">
+
+                    <div class="cflt">
+                        <img src="{{ asset('assets/images/courses-image5.jpg') }}" alt="" width="100%" height="170px">
+                    </div>
+
+                    <div class="courses-detail">
+                        <h3><a href="cflt">CFLT</a></h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur
+                            adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit
+                            amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+
+                    <div class="courses-info">
+                        <div class="courses-author">
+                            <img src="{{ asset('assets/images/author-image1.jpg') }}" alt="">
+                            <span>General</span>
+                        </div>
+                        <div class="courses-price">
+                            <a href="#"><span>Register</span></a>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+
         </div>
-   
+
+    </div>
+
+
+
+
+
+    {{-- ----------Section two---------- --}}
+
+
+    <div class="courses-section-2">
+        <div class="container courses-section-2-wrapper">
+
+            <div class="courses-item-1">
+                Is your institution interested in being onboarded as well? <span>Contact us</span> for more information.
+            </div>
+            <div class="courses-button-1">
+                <a href="#"> Register</a>
+
+            </div>
+        </div>
+    </div>
+
+    {{-- ----------Section Three---------- --}}
+
+    <div class="container courses-section-3-wrapper">
+        <div class="row" style="  text-align:center; margin-top: 40px; margin-bottom: 40px">
+            <h1>Assessments And Scoring Policy</h1>
+
+        </div>
+        <div class="row">
+            <div class="col-md-6 ">
+                <div class="cs3-item1">
+                    <img src="{{ asset('assets/images/2.png') }}" alt="" width="150px" height="130px">
+                    <div class="">
+                        <h2 style="margin-top: 0px; margin-bottom: 0px; padding-left: 12px;">Online Assessment
+                        </h2>
+                        <hr style="border: 2px solid white; margin-top: 0px; margin-bottom: 0px">
+                        <p style="padding-left: 12px;">At the end of each module you are asked to pass the final module
+                            assessment with a score of at
+                            least 60%. In case of not passing, you have a final second attempt to retake the assessment
+                            after
+                            studying the module again.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-md-6 ">
+                <div class="cs3-item1" style="padding-bottom: 20px">
+                    <img src="{{ asset('assets/images/3.png') }}" alt="" width="150px" height="130px">
+                    <div class="">
+                        <h2 style="margin-top: 0px; margin-bottom: 0px; padding-left: 12px;">Certification
+                        </h2>
+                        <hr style="border: 2px solid white; margin-top: 0px; margin-bottom: 0px">
+                        <p style="padding-left: 12px;">Your certificate will be issued after having completed all
+                            assessments successfully.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="row">
+            <div class="cs3-item2">
+                <img src="{{ asset('assets/images/1.png') }}" alt="" width="150px" height="130px">
+                <div class="">
+                    <h2 style="margin-top: 0px; margin-bottom: 0px; padding-left: 12px;">Final Teaching Assessment (For
+                        Trainers Only)
+                    </h2>
+                    <hr style="border: 2px solid white; margin-top: 0px; margin-bottom: 0px">
+                    <p style="padding-left: 12px;">At the end of each module
+                        you are asked to pass the final module
+                        assessment with a score of at
+                        least 60%. In case of not passing, you have a final second attempt to retake the assessment
+                        after
+                        studying the module again.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
     </div>
 @endsection
-
