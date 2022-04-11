d94920629839b9a4de9edb78e171390ec6c550db
DAMILARE1012 <dammy4did@gmail.com>
2022-04-11 13:29:02 +0100

    adfadfa

--- resources/views/roots/_css.blade.php
+++ resources/views/roots/_css.blade.php
@@ -1,12 +1,12 @@
-     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-     <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}">
-     <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.css') }}">
-     <link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css') }}">
-     <link rel="stylesheet" href="{{ asset('assets/css/carousel.css') }}">
-     <link rel="stylesheet" href="{{ asset('assets/css/blog.css') }}">
+     <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap.min.css') }}">
+     <link rel="stylesheet" href="{{ asset('/assets/css/font-awesome.min.css') }}">
+     <link rel="stylesheet" href="{{ asset('/assets/css/owl.carousel.css') }}">
+     <link rel="stylesheet" href="{{ asset('/assets/css/owl.theme.default.min.css') }}">
+     <link rel="stylesheet" href="{{ asset('/assets/css/carousel.css') }}">
+     <link rel="stylesheet" href="{{ asset('/assets/css/blog.css') }}">
 
      <!-- MAIN CSS -->
-     <link rel="stylesheet" href="{{ asset('assets/css/templatemo-style.css') }}">
+     <link rel="stylesheet" href="{{ asset('/assets/css/templatemo-style.css') }}">
      <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@2.8.2/dist/alpine.min.js"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	 
--- resources/views/roots/_javascript.blade.php
+++ resources/views/roots/_javascript.blade.php
@@ -1,12 +1,12 @@
 <!-- SCRIPTS -->
-<script src="{{ asset('assets/js/backtotop.js') }}"></script>
-<script src="{{ asset('assets/js/jquery.js') }}"></script>
-<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('assets/js/smoothscroll.js') }}"></script>
-<script src="{{ asset('assets/js/custom.js') }}"></script>
-<script src="{{ asset('assets/js/videoPlay.js') }}"></script>
-<script src="{{ asset('assets/js/carousel.js') }}"></script>
+<script src="{{ asset('/assets/js/backtotop.js') }}"></script>
+<script src="{{ asset('/assets/js/jquery.js') }}"></script>
+<script src="{{ asset('/assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('/assets/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('/assets/js/smoothscroll.js') }}"></script>
+<script src="{{ asset('/assets/js/custom.js') }}"></script>
+<script src="{{ asset('/assets/js/videoPlay.js') }}"></script>
+<script src="{{ asset('/assets/js/carousel.js') }}"></script>
 
 <script type="text/javascript">
 
